84d25f8ec6d507101563edd1560fb7e822f6be39
Sakthivel <sakthi.samuthiram@gmail.com>
2024-04-02 11:36:46 +0530

    Format,linting and husky setup

--- src/factorial.js
+++ src/factorial.js
@@ -1,6 +1,6 @@
-function factorial(n) {
-  if (n == 0 || n == 1) return 1;
-  return n * factorial(n - 1);
+function factorial (n) {
+  if (n === 0 || n === 1) return 1
+  return n * factorial(n - 1)
 }
 
-export default factorial;
+export default factorial
--- tests/factorial.test.js
+++ tests/factorial.test.js
@@ -1,21 +1,21 @@
-import { it, describe, expect } from "vitest";
-import factorial from "../src/factorial";
+import { it, describe, expect } from 'vitest'
+import factorial from '../src/factorial'
 
-describe("factorial", () => {
-  it("should return 1, for factorial of 0 or 1", () => {
-    expect(factorial(0)).toBe(1);
-    expect(factorial(1)).toBe(1);
-  });
+describe('factorial', () => {
+  it('should return 1, for factorial of 0 or 1', () => {
+    expect(factorial(0)).toBe(1)
+    expect(factorial(1)).toBe(1)
+  })
 
-  it("should return 2 for factorial of 2", () => {
-    expect(factorial(2)).toBe(2);
-  });
+  it('should return 2 for factorial of 2', () => {
+    expect(factorial(2)).toBe(2)
+  })
 
-  it("should return 6 for factorial of 3", () => {
-    expect(factorial(3)).toBe(6);
-  });
+  it('should return 6 for factorial of 3', () => {
+    expect(factorial(3)).toBe(6)
+  })
 
-  it("should return 120 for factorial of 5", () => {
-    expect(factorial(5)).toBe(120);
-  });
-});
+  it('should return 120 for factorial of 5', () => {
+    expect(factorial(5)).toBe(120)
+  })
+})
